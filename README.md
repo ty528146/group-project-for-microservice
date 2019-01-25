760b4c08708ad9f3e75267b361d3578dbe54edc8
tian <yt2583columbia.edu@dyn-160-39-244-139.dyn.columbia.edu>
2019-01-24 20:14:44 -0500

    add version for ajax

--- README.md
+++
@@ -1,3 +0,0 @@
-# group-project-for-microservice
-group project for microservice -- COMS 6156
-this is my frst coomit based on the you tube https://www.youtube.com/watch?v=gTowbsNPp9I&index=6&list=PLpPnRKq7eNW3Qm2OfoJ3Hyvf-36TulLDp
--- helloworld.js
+++
@@ -1,5 +0,0 @@
-document.write("a simple js project");
-var friends = ["Mike", "Stacy", "Andy", "Rick"];
-friends.forEach(function (eachName, index){
-    console.log(index + 1 + ". " + eachName); // 1. Mike, 2. Stacy, 3. Andy, 4. Rick​
-});
